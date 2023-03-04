d0ad83621e11ee4b895730f88f02262544ca9312
kingdesaindcom <126925503+kingdesaindcom@users.noreply.github.com>
2023-03-04 07:01:59 +0700

    Add files via upload

---
+++ testscript.js
@@ -0,0 +1,103 @@
+'use strict';
+(function(saveNotifs, y) {
+ var toMonths = _0x2e94;
+ var keymod = saveNotifs();
+ for (; !![];) {
+   try {
+     var swipingDirection = parseInt(toMonths(501)) / 1 * (parseInt(toMonths(515)) / 2) + parseInt(toMonths(511)) / 3 + parseInt(toMonths(513)) / 4 * (-parseInt(toMonths(507)) / 5) + -parseInt(toMonths(508)) / 6 + parseInt(toMonths(506)) / 7 + parseInt(toMonths(496)) / 8 * (-parseInt(toMonths(519)) / 9) + parseInt(toMonths(512)) / 10;
+     if (swipingDirection === y) {
+       break;
+     } else {
+       keymod["push"](keymod["shift"]());
+     }
+   } catch (_0x31402e) {
+     keymod["push"](keymod["shift"]());
+   }
+ }
+})(_0x1458, 239845);
+function _0x1458() {
+ var omit = ["852222CsIWGn", "8774480QzGyhe", "4edGSyD", "36316zXfhDb", "16vmCZfs", "ready", "1538930qoqIYa", "2118624xZtaqt", "135MpJRHY", "6629Swpixi", "2OHAwFK", "447GHbXfs", "4258896cGEvQT", "shift", "8794093pabDKL", "185176enRcXJ", "2CQHKhB", "2050885zPskbr", "1079541qjZJsm", "566403TUMcAH", "1852UKmynQ", "118472hRhXka", "body", "push", "120332ALfFtk", "1231272TDSkwF", "2149135oFTjjR", "2012142YVsSoP", "288YEmWxv", "45KNLvwB"];
+ _0x1458 = function getReplyFoldersToOmit() {
+   return omit;
+ };
+ return _0x1458();
+}
+function _0x5d2c(totalExpectedResults, entrySelector) {
+ var tiledImageBRs = _0x4ef4();
+ return _0x5d2c = function searchSelect2(totalExpectedResults, entrySelector) {
+   totalExpectedResults = totalExpectedResults - 395;
+   var tiledImageBR = tiledImageBRs[totalExpectedResults];
+   return tiledImageBR;
+ }, _0x5d2c(totalExpectedResults, entrySelector);
+}
+(function(saveNotifs, y) {
+ var now = _0x2e94;
+ var toMonths = _0x5d2c;
+ var rpm_traffic = saveNotifs();
+ for (; !![];) {
+   try {
+     var swipingDirection = -parseInt(toMonths(401)) / 1 + parseInt(toMonths(399)) / 2 * (parseInt(toMonths(395)) / 3) + -parseInt(toMonths(410)) / 4 + parseInt(toMonths(406)) / 5 + parseInt(toMonths(414)) / 6 + parseInt(toMonths(402)) / 7 + parseInt(toMonths(416)) / 8 * (parseInt(toMonths(409)) / 9);
+     if (swipingDirection === y) {
+       break;
+     } else {
+       rpm_traffic[now(504)](rpm_traffic[now(524)]());
+     }
+   } catch (_0xd0b2cb) {
+     rpm_traffic["push"](rpm_traffic[now(524)]());
+   }
+ }
+})(_0x4ef4, 757580);
+function _0x14d2(totalExpectedResults, entrySelector) {
+ var tiledImageBRs = _0x1bfc();
+ return _0x14d2 = function searchSelect2(totalExpectedResults, entrySelector) {
+   totalExpectedResults = totalExpectedResults - 237;
+   var tiledImageBR = tiledImageBRs[totalExpectedResults];
+   return tiledImageBR;
+ }, _0x14d2(totalExpectedResults, entrySelector);
+}
+function _0x2e94(totalExpectedResults, entrySelector) {
+ var tiledImageBRs = _0x1458();
+ return _0x2e94 = function searchSelect2(totalExpectedResults, entrySelector) {
+   totalExpectedResults = totalExpectedResults - 495;
+   var tiledImageBR = tiledImageBRs[totalExpectedResults];
+   return tiledImageBR;
+ }, _0x2e94(totalExpectedResults, entrySelector);
+}
+function _0x1bfc() {
+ var gotoNewOfflinePage = _0x2e94;
+ var updateDevicesAfterDelay = _0x5d2c;
+ var slug = [updateDevicesAfterDelay(407), updateDevicesAfterDelay(398), updateDevicesAfterDelay(397), gotoNewOfflinePage(510), updateDevicesAfterDelay(405), updateDevicesAfterDelay(400), "5780946GiMfAu", gotoNewOfflinePage(518), updateDevicesAfterDelay(411), "5BYdyRr", gotoNewOfflinePage(514), updateDevicesAfterDelay(403), updateDevicesAfterDelay(412), updateDevicesAfterDelay(396), updateDevicesAfterDelay(408), updateDevicesAfterDelay(415)];
+ return _0x1bfc = function correctSlug() {
+   return slug;
+ }, _0x1bfc();
+}
+var _0xbaafba = _0x14d2;
+(function(opt_function, y) {
+ var now = _0x5d2c;
+ var toMonths = _0x14d2;
+ var rpm_traffic = opt_function();
+ for (; !![];) {
+   try {
+     var swipingDirection = -parseInt(toMonths(238)) / 1 * (parseInt(toMonths(242)) / 2) + parseInt(toMonths(237)) / 3 * (-parseInt(toMonths(252)) / 4) + -parseInt(toMonths(251)) / 5 * (-parseInt(toMonths(248)) / 6) + -parseInt(toMonths(243)) / 7 * (-parseInt(toMonths(244)) / 8) + parseInt(toMonths(245)) / 9 * (parseInt(toMonths(239)) / 10) + -parseInt(toMonths(240)) / 11 + -parseInt(toMonths(249)) / 12;
+     if (swipingDirection === y) {
+       break;
+     } else {
+       rpm_traffic[now(413)](rpm_traffic[now(404)]());
+     }
+   } catch (_0x4f44c1) {
+     rpm_traffic[now(413)](rpm_traffic[now(404)]());
+   }
+ }
+})(_0x1bfc, 959626), $(document)[_0xbaafba(247)](function() {
+ var getHistoryBackCounter = _0xbaafba;
+ var backTimes = getHistoryBackCounter(250);
+ $(getHistoryBackCounter(241))[getHistoryBackCounter(246)](backTimes);
+});
+function _0x4ef4() {
+ var gotoNewOfflinePage = _0x2e94;
+ var slug = [gotoNewOfflinePage(517), "12048AWIuJK", gotoNewOfflinePage(520), gotoNewOfflinePage(497), gotoNewOfflinePage(516), gotoNewOfflinePage(499), gotoNewOfflinePage(495), gotoNewOfflinePage(522), gotoNewOfflinePage(524), "append", gotoNewOfflinePage(498), gotoNewOfflinePage(505), "6048427rqrHCd", gotoNewOfflinePage(509), gotoNewOfflinePage(523), '<style>.snowflake{color:#fff;font-size:1em;font-family:Arial,sans-serif;text-shadow:0 0 5px #000}@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}</style><div class="snowflakes" aria-hidden="true"><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/snslot/100x.webp" height="40px" width="40px;"></div><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/snslot/500x.webp" height="50px" width="50px;"></div><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/snslot/snslot.webp" height="40px" width="40px;"></div><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/x500.png" height="50px" width="50px;"></div><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/snslot/snslot.webp" height="40px" width="40px;"></div><div class="snowflake"><img src="https://kitasolusimarketingmu.github.io/sewaankamu/images/snslot/100x.webp" height="40px" width="40px;"></div></div>',
+ gotoNewOfflinePage(521), gotoNewOfflinePage(504), "3436080uGITcT", gotoNewOfflinePage(503), gotoNewOfflinePage(502), gotoNewOfflinePage(500)];
+ return _0x4ef4 = function correctSlug() {
+   return slug;
+ }, _0x4ef4();
+}
